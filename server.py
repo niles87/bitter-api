4f4510dd3ff80e1b8310e87173e4aa9a0645a071
Niles87 <nbing19@gmail.com>
2021-02-02 18:55:52 -0800

    added routes with basic return

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request, Response
 import firebase_admin
 from firebase_admin import credentials, db
 from dotenv import load_dotenv, find_dotenv
@@ -15,9 +15,35 @@ firebase_admin.initialize_app(cred, {
 app = Flask(__name__)
 app.config.from_object('config.Development')
 
+
 @app.route('/posts', methods=['GET'])
-def posts():
+def allPosts():
     return "<h1>Hello world</h1>"
+
+
+@app.route('/post')
+def getPost():
+    user = request.args['user']
+    post = request.args['post']
+    return '''
+            <h1>user is {}</h1>
+            <h1>post is {}</h1>'''.format(user, post)
+
+
+@app.route('/posts', methods=['POST'])
+def addPost():
+    return "post added"
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    return True
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return True
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
