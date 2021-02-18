9c7569c18fe387a6eb02d08ed9e7ac255dcdc1ad
Niles87 <nbing19@gmail.com>
2021-02-17 19:42:02 -0800

    added schemas for mongoengine

---
+++ models.py
@@ -0,0 +1,22 @@
+from mongoengine import *
+from datetime import datetime
+
+class User(Document):
+    username = StringField(max_length=20, required=True)
+    email = StringField(max_length=50, required=True)
+    password = StringField(required=True)
+    image = StringField(required=True)
+    name = StringField(max_length=50, required=True)
+
+
+class Comment(EmbeddedDocument):
+    comment = StringField(required=True, max_length=150)
+    author = ReferenceField(User)
+    date_time = DateTimeField(default=datetime.utcnow)
+
+  
+class Bit(Document):
+    author = ReferenceField(User, reverse_delete_rule=CASCADE, required=True)
+    bit = StringField(required=True, max_length=240)
+    date_time = DateTimeField(default=datetime.utcnow)
+    comments = ListField(EmbeddedDocumentField(Comment))
